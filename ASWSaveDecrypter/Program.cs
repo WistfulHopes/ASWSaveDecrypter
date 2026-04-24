8317f3addfe722971f1249a276d71f9cd46f576e
WistfulHopes <kp72002@gmail.com>
2026-04-23 19:48:45 -0700

    encryption support

--- ASWSaveDecrypter/Program.cs
+++ ASWSaveDecrypter/Program.cs
@@ -8,33 +8,62 @@ using AesProvider = System.Security.Cryptography.Aes;
 if (args.Length < 3)
 {
     Console.WriteLine("Usage:" +
-                      "ASWSaveDecrypter <key> <inputFile> <outputFile>");
+                      "ASWSaveDecrypter <key> <inputFile> <outputFile> <encrypt (optional)>");
     return;
 }
 
-const int blockSize = 16 * 8;
+if (args.Length == 3)
+{    
+    const int blockSize = 16 * 8;
 
-var provider = AesProvider.Create();
-provider.Mode = CipherMode.ECB;
-provider.Padding = PaddingMode.None;
-provider.BlockSize = blockSize;
+    var provider = AesProvider.Create();
+    provider.Mode = CipherMode.ECB;
+    provider.Padding = PaddingMode.None;
+    provider.BlockSize = blockSize;
 
-var key = Encoding.ASCII.GetBytes(args[0]).Take(32).ToArray();
+    var key = Encoding.ASCII.GetBytes(args[0]).Take(32).ToArray();
 
-var file = File.ReadAllBytes(args[1]);
+    var file = File.ReadAllBytes(args[1]);
 
-var decrypt = provider.CreateDecryptor(key, null).TransformFinalBlock(file, 0, file.Length);
-using var ds = new ZLibStream(new MemoryStream(decrypt), CompressionMode.Decompress);
-using var sr = new BinaryReader(ds);
-const int bufferSize = 4096;
-using var ms = new MemoryStream();
-var buffer = new byte[bufferSize];
-int count;
-while ((count = sr.Read(buffer, 0, buffer.Length)) != 0)
-    ms.Write(buffer, 0, count);
+    var decrypt = provider.CreateDecryptor(key, null).TransformFinalBlock(file, 0, file.Length);
+    using var ifs = new ZLibStream(new MemoryStream(decrypt), CompressionMode.Decompress);
+    using var sr = new BinaryReader(ifs);
+    const int bufferSize = 4096;
+    using var ms = new MemoryStream();
+    var buffer = new byte[bufferSize];
+    int count;
+    while ((count = sr.Read(buffer, 0, buffer.Length)) != 0)
+        ms.Write(buffer, 0, count);
 
-var buf = ms.ToArray();
+    var buf = ms.ToArray();
 
-var final = provider.CreateDecryptor(key, null).TransformFinalBlock(buf, 0, buf.Length);
+    var final = provider.CreateDecryptor(key, null).TransformFinalBlock(buf, 0, buf.Length);
 
-File.WriteAllBytes(args[2], final);
+    File.WriteAllBytes(args[2], final);
+}
+else 
+{
+    const int blockSize = 16 * 8;
+
+    var provider = AesProvider.Create();
+    provider.Mode = CipherMode.ECB;
+    provider.Padding = PaddingMode.Zeros;
+    provider.BlockSize = blockSize;
+
+    var key = Encoding.ASCII.GetBytes(args[0]).Take(32).ToArray();
+
+    var file = File.ReadAllBytes(args[1]);
+
+    var encrypt = provider.CreateEncryptor(key, null).TransformFinalBlock(file, 0, file.Length);
+    using var ms = new MemoryStream();
+    using (var ds = new ZLibStream(ms, CompressionLevel.Optimal))
+    {
+        ds.Write(encrypt, 0, encrypt.Length);
+    }
+
+    var buf = ms.ToArray();
+
+    var final = provider.CreateEncryptor(key, null).TransformFinalBlock(buf, 0, buf.Length);
+
+    File.WriteAllBytes(args[2], final);
+}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ASWSaveDecrypter
 
-Usage: ASWSaveDecrypter <key> <inputFile> <outputFile>
+Usage: ASWSaveDecrypter <key> <inputFile> <outputFile> <encrypt (optional)>
 
 Get the key from (pak)/RED/Config/Windows/WindowsEngine.ini
